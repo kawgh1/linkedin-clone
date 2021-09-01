f32fb64946b8df2ace9b88983ed63451df73d040
kawgh1 <krseven@gmail.com>
2021-09-01 01:23:41 -0500

    implemented Header component, added HeaderOption Component, styling, Material UI Icons and Avatar

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import './Header.css'
 import SearchIcon from '@material-ui/icons/Search';
+import HeaderOption from './HeaderOption';
+import HomeIcon from '@material-ui/icons/Home'
+import SupervisorAccountIcon from '@material-ui/icons/SupervisorAccount'
+import BusinessCenterIcon from '@material-ui/icons/BusinessCenter'
+import ChatIcon from '@material-ui/icons/Chat'
+import NotificationsIcon from '@material-ui/icons/Notifications'
 
 function Header() {
     return (
         <div className='header'>
-            <h1>This is the header</h1>
 
             <div className="header__left">
 
@@ -18,6 +23,12 @@ function Header() {
             </div>
 
             <div className="header__right">
+                <HeaderOption Icon={HomeIcon}title='Home' />
+                <HeaderOption Icon={SupervisorAccountIcon}title='My Network' />
+                <HeaderOption Icon={BusinessCenterIcon}title='Jobs' />
+                <HeaderOption Icon={ChatIcon}title='Messaging' />
+                <HeaderOption Icon={NotificationsIcon}title='Notifications' />
+                <HeaderOption avatar='https://compassionate-leakey-e9b16b.netlify.app/images/IG_Sonny.jpeg' title='me'/>
 
             </div>
         </div>
