edf190e5ffafec2cfa0968cb65742f3c3ca7151a
kawgh1 <krseven@gmail.com>
2021-09-02 22:56:28 -0500

    added Post Component, styling

--- src/Feed.js
+++ src/Feed.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './Feed.css'
 import CreateIcon from '@material-ui/icons/Create'
 import InputOption from './InputOption'
@@ -6,8 +6,11 @@ import ImageIcon from '@material-ui/icons/Image'
 import SubscriptionsIcon from '@material-ui/icons/Subscriptions'
 import EventNoteIcon from '@material-ui/icons/EventNote'
 import CalendarViewDayIcon from '@material-ui/icons/CalendarViewDay'
+import Post from './Post'
 
 function Feed() {
+
+    const [post, setPosts] = useState([]);
     return (
         <div className='feed'>
 
@@ -27,12 +30,13 @@ function Feed() {
                     <InputOption title='Photo' Icon={ImageIcon} color='#70B5F9'/>
                     <InputOption title='Video' Icon={SubscriptionsIcon} color='#e7a33e'/>
                     <InputOption title='Event' Icon={EventNoteIcon} color='#c0cbcd'/>
-                    <InputOption title='Write Article' Icon={CalendarViewDayIcon} color='#7fc15e'/>
+                    <InputOption title='Write' Icon={CalendarViewDayIcon} color='#7fc15e'/>
                 </div>
 
             </div>
 
-           
+           {/* FEED POSTS */}
+            <Post name='Sonny Sangha' description='this is a test' message='this is a message'  />
             
         </div>
     )
---
+++ src/Post.js
@@ -0,0 +1,39 @@
+import { Avatar } from '@material-ui/core'
+import React from 'react'
+import './Post.css'
+import ThumbUpOutlinedIcon from '@material-ui/icons/ThumbUpOutlined';
+import ChatOutlinedIcon from '@material-ui/icons/ChatOutlined';
+import SendOutlinedIcon from '@material-ui/icons/SendOutlined';
+import ShareOutlinedIcon from '@material-ui/icons/ShareOutlined';
+import InputOption from './InputOption';
+
+function Post({name, description, message, photoUrl}) {
+    return (
+        <div className='post'>
+            <div className="post__header">
+                <Avatar />
+                <div className="post__info">
+                    <h2>{name}</h2>
+                    <p>{description}</p>
+                </div>
+
+            </div>
+
+            <div className="post__body">
+                <p>{message}</p>
+            </div>
+
+                <div className="post__buttons">
+                    <InputOption title='Like' Icon={ThumbUpOutlinedIcon} color='gray' />
+                    <InputOption title='Comment' Icon={ChatOutlinedIcon} color='gray' />
+                    <InputOption title='Share' Icon={ShareOutlinedIcon} color='gray' />
+                    <InputOption title='Send' Icon={SendOutlinedIcon} color='gray' />
+                </div>
+            
+            
+            
+        </div>
+    )
+}
+
+export default Post
